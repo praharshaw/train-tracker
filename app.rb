ebdbd4a7951d60d89e0ea4c9135b50848694b802
wenderen <rohan.prinja@gmail.com>
2013-07-14 08:13:52 +0530

    Wooooooooooooo

--- app.rb
+++ app.rb
@@ -29,6 +29,8 @@ get '/info' do
     @train_date = train_json["tdate"]
     @train_date_reversed = @train_date.split('-').reverse.join('-')
 
+    @final_destn = train_json["to"]
+
     uri = URI.parse("http://coa-search-193678880.ap-southeast-1.elb.amazonaws.com/search.json?q=#{@train_num}")
     response = Net::HTTP.get_response(uri)
 
@@ -39,11 +41,8 @@ get '/info' do
 
     @stations_hash = Hash[abbrev_stations.zip(full_stations)]
 
-    @next_stn = call_curl(abbrev_stations.join("%2C"))
-
-    @prev_stn=''
-    # @next_stn=''
-    @final_destn=''
+    set_next_and_prev(abbrev_stations.join("%2C"))
+    set_final_long
 
     @weather_forecast, @distance_to_destn = weather_distance_api
 
@@ -52,29 +51,45 @@ get '/info' do
 end
 
 helpers do
-  def call_curl(stns)
-    text_dump = `curl 'http://trainenquiry.com/RailYatri.ashx' -H 'Cookie: ASP.NET_SessionId=av2jvi45kkxgf1zemhvoe3fu; __gads=ID=da9366776debb721:T=1373759777:S=ALNI_MbTBC-RdvvVfSAJhZ5PqTylQHUoSA; OX_plg=swf|qt|wmp|shk|pm; __utma=177604064.1792347361.1373759770.1373759770.1373759770.1; __utmb=177604064.5.10.1373759770; __utmc=177604064; __utmz=177604064.1373759770.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)' -H 'Origin: http://trainenquiry.com' -H 'Accept-Encoding: gzip,deflate,sdch' -H 'Host: trainenquiry.com' -H 'Accept-Language: en-US,en;q=0.8' -H 'User-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.71 Safari/537.36' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Accept: */*' -H 'Referer: http://trainenquiry.com/TrainStatus.aspx' -H 'X-Requested-With: XMLHttpRequest' -H 'Connection: keep-alive' --data 't=15160&s=2013-07-13&codes=DURG%2CBPHB%2CR%2CTLD%2CBYT%2CBYL%2CBSP%2CUSL%2CKGB%2CBIG%2CPND%2CAPR%2CAAL%2CBUH%2CSDL%2CBRS%2CUMR%2CCHD%2CKTE%2CMYR%2CUHR%2CSTA%2CJTW%2CMJG%2CMKP%2CDBR%2CSRJ%2CNYN%2CALD%2CPLP%2CJNH%2CBOY%2CBSB%2CBCY%2CARJ%2CGCT%2CYFP%2CCBN%2CBUI%2CSTW%2CSIP%2CCPR&RequestType=Location' --compressed`
+  def set_next_and_prev(stns)
+
+    # grep_result = `curl --head 'http://trainenquiry.com/RailYatri.ashx' | grep Set-Cookie`
+    # cookie = grep_result.split[1]
+
+    `curl --dump-header headers_and_cookies http://trainenquiry.com/ 1>/dev/null 2>/dev/null`
+    cookie_line = `grep Set-Cookie headers_and_cookies`
+    cookie = cookie_line.split[1]
+
+    text_dump = `curl 'http://trainenquiry.com/RailYatri.ashx' -H 'Cookie: #{cookie} __gads=ID=da9366776debb721:T=1373759777:S=ALNI_MbTBC-RdvvVfSAJhZ5PqTylQHUoSA; OX_plg=swf|qt|wmp|shk|pm; __utma=177604064.1792347361.1373759770.1373759770.1373759770.1; __utmb=177604064.5.10.1373759770; __utmc=177604064; __utmz=177604064.1373759770.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)' -H 'Origin: http://trainenquiry.com' -H 'Accept-Encoding: gzip,deflate,sdch' -H 'Host: trainenquiry.com' -H 'Accept-Language: en-US,en;q=0.8' -H 'User-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.71 Safari/537.36' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Accept: */*' -H 'Referer: http://trainenquiry.com/TrainStatus.aspx' -H 'X-Requested-With: XMLHttpRequest' -H 'Connection: keep-alive' --data 't=#{@train_num}&s=#{@train_date_reversed}&codes=#{stns}&RequestType=Location' --compressed`
 
     json = JSON.load text_dump
-    return json[json["keys"][0]]["station_updates"].reject { |k,v| v["status"] == "departed" }.keys.first
+
+    @next_stn = json[json["keys"][0]]["station_updates"].reject { |k,v| v["status"] == "departed" }.keys.first
+    @prev_stn = json[json["keys"][0]]["station_updates"].reject { |k,v| v["status"] == "not_reached" }.keys.last
+    @prev_stn = (@prev_stn.nil? or @prev_stn == "nil" or @prev_stn == "") ? "Your train hasn't started yet" : @prev_stn
   end
 
   def weather_distance_api
+    unless @next_stn.nil? or @final_destn.nil?
+      next_stn_long = @stations_hash[@next_stn].split.first
+      final_destn_long = @stations_hash[@final_destn].split.first
 
-    next_stn_long = @stations_hash[@next_stn]
-    final_destn_long = @stations_hash[@final_destn]
+      distance_hash = JSON.load(`curl 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20geo.distance%20where%20place1%3D%22#{next_stn_long}%22%20and%20place2%3D%22#{final_destn_long}%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'`)
 
-    distance_hash = JSON.load(`curl 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20geo.distance%20where%20place1%3D%22#{next_stn_long}%22%20and%20place2%3D%22#{final_destn_long}%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'`)
+      next_woeid = distance_hash["query"]["results"]["distance"]["place"][0]["locality1"]["woeid"]
+      dest_woeid = distance_hash["query"]["results"]["distance"]["place"][1]["locality1"]["woeid"]
 
-    next_woeid = distance_hash["query"]["results"]["distance"]["place"][0]["locality1"]["woeid"]
-    dest_woeid = distance_hash["query"]["results"]["distance"]["place"][1]["locality1"]["woeid"]
+      distance = distance_hash["query"]["results"]["distance"]["kilometers"]
 
-    distance = distance_hash["query"]["results"]["distance"]["kilometers"]
+      weather_hash = JSON.load(`curl 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D#{next_woeid}&format=json&diagnostics=true'`)
 
-    weather_hash = JSON.load(`curl 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D#{next_woeid}&format=json&diagnostics=true'`)
+      forecast = weather_hash["query"]["results"]["channel"]["item"]["forecast"][0]["text"]
 
-    forecast = weather_hash["query"]["results"]["channel"]["item"]["forecast"][0]["text"]
+      return [forecast, distance]
+    end
+  end
 
-    return [forecast, distance]
+  def set_final_long
+    @final_destn_long = @stations_hash[@final_destn]
   end
 end
